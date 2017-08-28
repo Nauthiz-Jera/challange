7b7762c4eb1bb1390670b1b2f1995f412fde6d65
Nauthiz-Jera <nauthiz.jera@gmail.com>
2017-08-28 18:16:24 -0400

    change to package json

--- app.js
+++ app.js
@@ -6,6 +6,13 @@ const logger = require('morgan');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 
+const app = express();
+
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
+
 /* Routes */
 const index = require('./routes/index');
 const users = require('./routes/users');
@@ -13,8 +20,6 @@ const addWidget = require('./routes/addWidget');
 const allWidgets = require('./routes/allWidgets');
 const updateQuantity = require('./routes/updateQuantity');
 const submitOrder = require('./routes/submitOrder');
-
-const app = express();
 
 const config = require('./config/config');
 
@@ -27,10 +32,6 @@ db.on('error', console.error.bind(console, 'connection error'));
 const staticFiles = express.static(path.join(__dirname, '../../client/build'));
 app.use(staticFiles);
 
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cookieParser());
 
 app.use('/', index);
 app.use('/users', users);
